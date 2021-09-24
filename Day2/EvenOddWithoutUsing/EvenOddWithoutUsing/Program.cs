d7ea3d6261120be689fd4f59d01bb4edbafa6fd9
Neha121995 <jagtapneha1212@gmail.com>
2021-09-24 23:15:46 +0530

    [Neha] added : Even odd number without using recursion programm

---
+++ Day2/EvenOddWithoutUsing/EvenOddWithoutUsing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EvenOddWithoutUsing
+{
+    class Program
+    {
+        static int isEven(int num) // 10
+        {
+            return (num & 1); // 0
+        }
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Enter a number ::");
+            int num = int.Parse(Console.ReadLine());
+            if (isEven(num) == 0) // 0 == 0
+                Console.Write("Number is Even");
+            else
+                Console.Write("Number is Odd");
+        }
+    }
+}
