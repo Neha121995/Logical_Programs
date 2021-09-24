af8cf9a9685c09f1fa74c8aa77038a52b0189b8e
Neha121995 <jagtapneha1212@gmail.com>
2021-09-24 23:08:34 +0530

    [Neha] added : Integer Operation using arithmetic operators

---
+++ Day2/IntOpt/IntOpt/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IntOpt
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter b : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter c : ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int result = a + b * c;
+            Console.WriteLine(" a + b * c = {0}",result);
+            int result1 = a * b + c;
+            Console.WriteLine(" a * b + c = {0}", result1);
+            int result2 = c + a / b;
+            Console.WriteLine(" c + a / b = {0}", result2);
+            int result3 = a % b + c;
+            Console.WriteLine(" a % b + c = {0}", result3);
+        }
+    }
+}
