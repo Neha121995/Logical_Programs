5be247f32267cad3880db790bc8a8c9b17a0a5d3
Neha121995 <jagtapneha1212@gmail.com>
2021-09-24 23:15:18 +0530

    [Neha] added : Even odd number using recursion programm

---
+++ Day2/EvenOddNumber/EvenOddNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EvenOddNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter any number ::");
+            int num = int.Parse(Console.ReadLine()); // 16
+
+            if ((num / 2)*2 == num) //16 / 2 = 8  //8*2 = 16 // 16==16
+            {
+                Console.WriteLine("Number is Even");
+            }else
+            {
+                Console.WriteLine("Number is Odd");
+            }
+        }
+    }
+}
