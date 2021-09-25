d6b7453e39c79f879cd989c5f184cb633bbef105
Neha121995 <jagtapneha1212@gmail.com>
2021-09-25 09:50:00 +0530

    [Neha] added : Sum of two dice roll

---
+++ Day2/SumTwoDice/SumTwoDice/Program.cs
@@ -0,0 +1,24 @@
+﻿using EllipticCurve.Utils;
+using System;
+
+namespace SumTwoDice
+{
+    class Program
+    {
+        public void AddDice()
+        {
+            Random random = new Random();
+            int DiceRoll1 = random.Next(1, 7);
+
+            Random random1 = new Random();
+            int DiceRoll2 = random.Next(1, 7);
+
+
+            int Sum = DiceRoll1 + DiceRoll2;
+            Console.WriteLine("sum of two dice Roll = " + Sum);
+
+
+            Console.ReadLine();
+        }
+    }
+}
