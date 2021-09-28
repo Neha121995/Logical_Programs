53c199f28f92468ea9ad4147a7ac9edcd43a9d36
Neha121995 <jagtapneha1212@gmail.com>
2021-09-29 00:03:46 +0530

    [Neha] added : Function to print harmonic number series

---
+++ Day4/HarmonicNumber/HarmonicNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HarmonicNumber
+{
+    class Program
+    {
+        public void findHarmonicSeries()
+        {
+            Console.WriteLine("Welcome to Harmonic series");
+
+            int Num;
+            Console.WriteLine("Enter Number till you want to print Harmonic series: ");
+            Num = Convert.ToInt32(Console.ReadLine());
+            double sumofHarmonicSeries = 0.0;
+
+            for (int i = 1; i <= Num; i++)
+            {
+
+                Console.Write("1/{0} +", i);
+                sumofHarmonicSeries = sumofHarmonicSeries + 1 / (double)i;
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("\nSum of series upto {0} terms : {1}\n", +Num, +sumofHarmonicSeries);
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Program harmonic = new Program();
+            harmonic.findHarmonicSeries();
+        }
+        }
+}
