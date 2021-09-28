9ee6d2554ad061e599d0202248eeb09ed3a4b9d5
Neha121995 <jagtapneha1212@gmail.com>
2021-09-29 00:09:49 +0530

    [Neha] added : Function to print power of two

---
+++ Day5/PowerOf2/PowerOf2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PowerOf2
+{
+    class Program
+    {
+        public void printPowerOfTwo()
+        {
+            int Num;
+            Console.WriteLine("Enter number till which you want to print power of 2 : ");
+            Num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            for (int i = 1; i <= Num; i++)
+            {
+                double Power = Math.Pow(2, i);
+                Console.WriteLine(Power);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.printPowerOfTwo();
+
+            Console.ReadLine();
+        }
+
+    }
+}
