2a712ee4691a5108148d733fe270e44b7aa9dd19
Neha121995 <jagtapneha1212@gmail.com>
2021-09-29 00:02:06 +0530

    [Neha] added : Function to find the windchill

---
+++ Day3/WindChill/WindChill/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace WindChill
+{
+    class Program
+    {
+        public void CalculateWindChill()
+        {
+            double Temperature;
+            Console.WriteLine("Enter temperature in fareheit= ");
+            Temperature = Convert.ToDouble(Console.ReadLine());
+
+            double WindSpeed;
+            Console.WriteLine("Enter the wind speed V = ");
+            WindSpeed = Convert.ToDouble(Console.ReadLine());
+
+
+            if (Temperature < 50 && (WindSpeed >= 3 && WindSpeed < 120))
+            {
+
+                double v = Math.Pow(WindSpeed, 0.16);
+
+                double WindChill = 35.74 + 0.6215 * Temperature + (0.4275 * Temperature - 35.75) * v;
+
+                Console.WriteLine("Windchill  = " + WindChill);
+            }
+            else
+            {
+                Console.WriteLine("Input is invalid");
+                CalculateWindChill();
+
+            }
+
+            Console.ReadLine();
+
+        }
+        static void main(String[] args)
+        {
+            Program w = new Program();
+            w.CalculateWindChill();
+        }
+
+    }
+}
