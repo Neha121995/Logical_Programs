437fa1e0f4956d61f56d573c5f9f0e64f2975b39
Neha121995 <jagtapneha1212@gmail.com>
2021-09-29 00:05:04 +0530

    [Neha] added : Function to find square root of non negative number

---
+++ Day4/Sqrt/Sqrt/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Sqrt
+{
+    class Program
+    {
+        public void caculateSquareRoot()
+        {
+
+            Console.WriteLine("Finding the square root of non-negative number C");
+            double C;
+            Console.WriteLine("Enter the input C");
+            C = Convert.ToDouble(Console.ReadLine());
+
+            double t = C;
+
+            double epsilon = 1e-15;
+
+            while (Math.Abs(t - C / t) > epsilon * t)
+            {
+
+                t = (C / t + t) / 2.0;
+            }
+
+            Console.WriteLine("The value of t is : " + t);
+            Console.ReadLine();
+
+        }
+
+        static void Main(string[] args)
+        {
+            Program sqrt = new Program();
+            sqrt.caculateSquareRoot();
+        }
+
+    }
+}
