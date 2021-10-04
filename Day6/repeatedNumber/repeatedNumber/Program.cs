4184fa850086697d879fce3ac8d92fe6532abf73
Neha121995 <jagtapneha1212@gmail.com>
2021-10-04 09:18:59 +0530

    [Neha] added : Function to find the repeated number

---
+++ Day6/repeatedNumber/repeatedNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace repeatedNumber
+{
+    class Program
+    {
+        public void FindRepeatedNumber()
+        {
+            Console.WriteLine("Enter array size : ");
+            int len = Convert.ToInt32(Console.ReadLine());
+
+            int[] numbers = new int[len];
+
+            Console.WriteLine("Enter Numbers : ");
+            for (int i = 0; i < len; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("Repeated elements are : ");
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = i + 1; j < len; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        Console.WriteLine(numbers[j]);
+                        break;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program repeatedNum = new Program();
+            repeatedNum.FindRepeatedNumber();
+        }
+    }
+}
